052a2179d39f446fd6ca3faa0043b39e4bc9e1ca
이은결 <lorenlee.dev@gmail.com>
2024-10-23 00:36:22 +0900

    refactor: SectionRecommendList 컴포넌트로 영역을 분리하고, 인라인 스타일을 modules로 분리

---
+++ src/ui/pages/Home/SectionRecommendList/index.tsx
@@ -0,0 +1,32 @@
+import {Button, Col, Flex, Popover, Row} from "antd";
+import Title from "antd/es/typography/Title";
+import Paragraph from "antd/es/typography/Paragraph";
+import {InfoCircleFilled, RightOutlined} from "@ant-design/icons";
+import JobList from "@/ui/components/organism/JobList";
+import Section from "@/ui/components/atom/Section";
+import styles from "@/ui/pages/Home/SectionRecommendList/SectionRecommendList.module.scss";
+
+const SectionRecommendList = ({data}) => {
+  return (
+    <Section>
+      <Row>
+        <Col span={24}>
+          <Flex gap={6} align={'center'} className={'mb-2'}>
+            <Title level={3}>맞춤 공고</Title>
+            <Popover content={
+              <Flex vertical={true}>
+                <p className={styles.description}>내가 작성한 이력서 기반으로 추천합니다.<br/>
+                  정확한 추천을 위해 이력서의 모든 항목을 입력해주세요!</p>
+                <Button>이력서 업데이트하기<RightOutlined className={styles.buttonIcon}/></Button>
+              </Flex>
+            } title="맞춤공고란?" trigger="click">
+              <InfoCircleFilled className={styles.triggerIcon}/>
+            </Popover>
+          </Flex>
+          <JobList data={data}/>
+        </Col>
+      </Row>
+    </Section>)
+}
+
+export default SectionRecommendList;
