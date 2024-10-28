a5c71cb854d20beac8a505174566e11de45c7956
이은결 <lorenlee.dev@gmail.com>
2024-10-28 21:05:00 +0900

    add: Footer 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import Navigation from "@/ui/components/organism/Navigation";
 import Layout from "@/ui/components/atom/Layout";
 import HomePage from "@/ui/pages/Home";
+import Footer from "@/ui/components/organism/Footer";
 
 export default function Home() {
   return (
     <Layout>
       <Navigation/>
       <HomePage/>
+      <Footer/>
     </Layout>
   );
 }
---
+++ src/ui/components/organism/Footer/index.tsx
@@ -0,0 +1,10 @@
+import styles from '@/ui/components/organism/Footer/Footer.module.scss';
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      Copyright © 2024 Between Jobs. All rights reserved.
+    </footer>
+  )
+}
+
+export default Footer;
