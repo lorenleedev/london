bf04c3a6f104850820065b3ffb795263125f0fcc
이은결 <lorenlee.dev@gmail.com>
2024-11-01 23:37:03 +0900

    chore: build 시점에 .env를 참조하도록 수정

--- src/thridparty/firebase.ts
+++ src/thridparty/firebase.ts
@@ -2,13 +2,13 @@ import {initializeApp} from "firebase/app";
 import {getAuth, GoogleAuthProvider} from "@firebase/auth";
 import { getDatabase } from "firebase/database";
 export const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MASSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
+  measurementId: process.env.measurementId
 };
 
 const app = initializeApp(firebaseConfig);
